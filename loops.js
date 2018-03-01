5aa00eedeb0701077b59adf49bb9ad295a30554a
Brandon Kim <bkim95ca@gmail.com>
2018-03-01 22:55:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,3 +3,7 @@ var gifts = ['teddy bear','drone', 'doll']
 function wrapGift(gift) {
   console.log('Wrapped ${gift} and added a bow')
 }
+
+wrapGift(gifts[0])
+wrapGift(gifts[1])
+wrapGift(gifts[2])
